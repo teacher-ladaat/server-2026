8b41fcb820f4b4598b7dfff54f8f75e1e81d7482
teacher-ladaat <aditeacherladaat@gmail.com>
2025-11-09 16:46:19 +0200

    add user router

---
+++ routes/user.route.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+
+const router = Router();
+
+// get all users
+router.get('/', (req, res) => {
+    res.send('get all users');
+});
+
+// register - add new user (regular POST)
+router.post('/', (req, res) => {
+    res.send('register');
+});
+
+// login - check exists user
+router.post('/login', (req, res) => {
+    res.send('login');
+});
+
+export default router;
