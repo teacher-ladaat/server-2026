63557e3bea138c67d7b7ce6e502aafa1ef400ca5
teacher-ladaat <aditeacherladaat@gmail.com>
2025-12-28 23:44:24 +0200

    implement user registration with error handling

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,18 @@
+import User from "../models/user.model.js";
+
 export const getAllUsers = (req, res) => {
     res.send('get all users, req time: ' + (new Date() - req.currentDate));
 };
 
-// TODO: register
-export const register = (req, res) => {
-    res.send('register');
+export const register = async (req, res, next) => {
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        res.status(201).json(newUser);
+    } catch (error) {
+        next({ status: 409, message: error.message });
+    }
 };
 
 export const login = (req, res) => {
